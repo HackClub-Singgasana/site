e973886de3f86cd67f781356425746afa84bf57a
Brian <bri4nong@gmail.com>
2020-07-18 17:36:03 +0700

    Add Source Code
    Leaders, resources, meetings, etc.

---
+++ build/main.js
@@ -0,0 +1,43 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var response = JSON.parse(this.responseText);
+        var leaders = response['leaders'];
+        var resources = response['resources'];
+        var nextMeeting = response['nextMeeting'];
+
+        // Load leaders
+        var countLeaders = leaders.length;
+        var leaderHTML = '';
+        var row = 0;
+        for (var i = 0; i < countLeaders; i++) {
+            leaderHTML += '<div class="leader"><img class="profile-image" src="' + leaders[i]['profilepic'] + '" width="70%"><br><b>' + leaders[i]['name'] + ' | ' + leaders[i]['ranks'] + '</b><p>' + leaders[i]['class'] + '</p><a href="https://github.com/' + leaders[i]['github'] + '" target="_blank">GitHub</a></div>';
+            row++;
+            if (row == 4 || i == countLeaders-1){
+                var leaderRow = '<div class="row">' + leaderHTML + '</div>';
+                console.log(leaderRow);
+                document.getElementById('leaders').innerHTML += leaderRow;
+                leaderHTML = '';
+                row = 0;
+            }
+        }
+
+        // Load resources
+        var countResources = resources.length;
+        for (var i = 0; i < countResources; i++) {
+            var resourceHTML = '<li><div class="resource"><a href="' + resources[i]['link'] + '" target="_blank"><img class="resource-image" src="' + resources[i]['thumbnail'] + '"></a><h4>' + resources[i]['name'] + '</h4><p>' + resources[i]['description'] + '</p></div></li>';
+            document.getElementById('resource-list').innerHTML += resourceHTML;
+        }
+
+        //Load next meeting info
+        var time = new Date(nextMeeting['time']);
+        var location = nextMeeting['location'];
+        var timeString = time.toUTCString() + '+7';
+        var meetingHTML = '<div title="Add to Calendar" class="addeventatc">Add to Calendar<span class="start">' + nextMeeting['time'] + '</span><span class="timezone">UTC+7</span><span class="title">Hack Club Singgasana Meeting</span><span class="description">https://singgasana.hackclub.com</span><span class="location">' + location + '</span></div>';
+        document.getElementById('time').innerHTML = '&#128197; ' + timeString;
+        document.getElementById('location').innerHTML = '&#128205; ' + location;
+        document.getElementById('meeting').innerHTML += meetingHTML;
+    }
+};
+xhttp.open("GET", ".netlify/functions/getAirtable/getAirtable.js", true);
+xhttp.send();
---
+++ functions/getAirtable/getAirtable.js
@@ -0,0 +1,86 @@
+
+// IMPORT THE AIRTABLE.JS PACKAGE
+const Airtable = require('airtable');
+
+/** THIS IS YOUR SERVERLESS FUNCTION */
+exports.handler = function(event, context, callback) {
+  //pull the required information from your environment variables, which can be set in the Netlify UI
+  const {API_URL, API_CLIENT_ID, API_KEY, DELAY} = process.env;
+
+  // THIS FUNCTION FORMATS AND SENDS YOUR RESPONSE BACK TO YOUR FRONT-END
+  const send = body => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(body)
+    });
+  }
+
+  // CONFIGURE YOUR AIRTABLE BASE CONNECTION
+  Airtable.configure({
+    endpointUrl: API_URL,
+    apiKey: API_KEY
+  });
+  var base = Airtable.base(API_CLIENT_ID);
+  
+  // Get leaders
+  function getLeaders() {
+    var leaders = new Array;
+    base('Leaders').select({sort: [{field: "Created Time", direction: "asc"}]}).eachPage(function page(records, fetchNextPage) {
+        records.forEach(function(record) {
+            var name = record.get('Name');
+            var classname = record.get('Class');
+            var github = record.get('GitHub');
+            var ranks = record.get('Ranks');
+            var profilePic = record.get('Profile Picture')[0]['url'];
+            leaders.push({"name": name, "class": classname, "github": github, "ranks": ranks, "profilepic": profilePic});
+        });
+        fetchNextPage();
+    }, function done(error) {
+        console.log(error);
+    });
+    return leaders;
+  }
+
+  // Get resources
+  function getResources() {
+    var resources = new Array;
+    base('Resources').select({sort: [{field: "Created Time", direction: "asc"}]}).eachPage(function page(records, fetchNextPage) {
+        records.forEach(function(record) {
+            var name = record.get('Name');
+            var description = record.get('Description');
+            var thumbnail = record.get('Thumbnail')[0]['url'];
+            var link = record.get('Link');
+            resources.push({"name": name, "description": description.replace(/\xA0/g, ' '), "thumbnail": thumbnail, "link": link});
+        });
+        fetchNextPage();
+    }, function done(error) {
+        console.log(error);
+    });
+    return resources;
+  }
+
+  // Get next meeting time and location
+  function getNextMeeting() {
+    var meetingInfo = new Array;
+    base('Meeting').select({maxRecords: 1, sort: [{field: "Created Time", direction: "desc"}]}).eachPage(function page(records, fetchNextPage) {
+        records.forEach(function(record) {
+            var time = record.get('Meeting Time');
+            var location = record.get('Location');
+            meetingInfo.push({"time": time, "location": location});
+        });
+        fetchNextPage();
+    }, function done(error) {
+        console.log(error);
+    });
+    return meetingInfo;
+  }
+
+  var leaders = getLeaders();
+  var resources = getResources();
+  var nextMeeting = getNextMeeting();
+
+  setTimeout(function () {
+    send({"leaders": leaders, "resources": resources, "nextMeeting": nextMeeting[0]});
+  }, parseInt(DELAY));
+  
+}
